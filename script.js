d9a6d5b7db3e5345d7414ece54714e214af0caea
nitrammm <109441438+nitrammm@users.noreply.github.com>
2022-09-06 10:12:25 -0500

    updated javascript with the future class and added id on text area in html

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 // document.getElementById("currentDay").append("hi")
 $("#currentDay").append(dayjs().format('dddd, MMMM D, YYYY'))
+var timeBlocks = $(".time-block")
+
+
+function coloringTheTextArea() {
+    var currentMilitaryTime = dayjs().format("H") // military time
+    console.log(currentMilitaryTime)
+    // $('#15').addClass("present") 
+    $(".description").each(function(){
+        console.log($(this))
+        console.log($(this).attr("id"))
+        if ($(this).attr("id") < currentMilitaryTime) {
+            $(this).addClass("past");
+        } else if ($(this).attr("id")  == currentMilitaryTime) {
+            $(this).addClass("present");
+        } else if ($(this).attr("id") > currentMilitaryTime) 
+            $(this).addClass("future");
+        
+    });
+    }
+
+    
+    for( var i = 0; i < timeBlocks.length; i++){
+        var oneTimeBlock = timeBlocks[i]
+        var time = oneTimeBlock.getAttribute("id")
+        var textAreaEl = oneTimeBlock.children[1]
+        console.log(textAreaEl)
+    }
+
+
+$(document).ready(function () {
+
+    $("button").on("click", function() {
+        localStorage.setItem();
+    });
+
+});
+
+coloringTheTextArea();
+
